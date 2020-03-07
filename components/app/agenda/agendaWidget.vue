167c0c2d92de1b599b8a8c10a20a59c3febcead8
sporkforcereal <juhojung@unomaha.edu>
2020-03-07 17:26:58 -0600

    created index 2

---
+++ components/app/agenda/agendaWidget.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-row class="font-weight-bold display-2 mb-10" justify="center">
+            AGENDA
+        </v-row>
+
+        <v-card height="100%" min-width="100%" class="accent pa-5">
+            <font color="white">
+                <p style="font-size:15px">hurry up and water your plants before they die</p>
+            </font>
+        </v-card>
+
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "agendaWidget"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,40 +1,16 @@
 <template>
     <v-container>
-        <v-row class="font-weight-bold display-2 mb-5" justify="center">
-            AGENDA
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 1
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 2
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 3
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 4
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 5
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 6
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 7
-        </v-row>
-        <v-row justify="center" class="mb-1">
-            to do 8
-        </v-row>
+        <agenda-widget/>
     </v-container>
 </template>
 
 
 
 <script>
+    import AgendaWidget from "../components/app/agenda/agendaWidget";
     export default {
         name: "agenda",
+        components: {AgendaWidget},
         layout: "appDefault",
     }
 </script>
