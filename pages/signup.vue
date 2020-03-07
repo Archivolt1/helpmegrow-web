30bdc04ef621093635440d1209708964b0288702
Matthew C. Tran <itgfreak911@gmail.com>
2020-03-07 17:24:55 -0600

    Signup page design.

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,92 +1,66 @@
 <template>
-    <v-container justify="center" class="" >
-            <v-row>
-                <v-col>
-                    <v-card height="508px" class="accent pa-5" >
-                        <font color="white"><br></br><br></br>
-                        <h1 style="font-size:40px;">Welcome to Help Me Grow</h1>
-                        <p style="font-size:15px;">The best place to keep track of your dying plants.</p>
-                        <div class="text-center"><v-btn>Already have an account?</v-btn></div>
-                        </font>
-                    </v-card>
-                </v-col>
-                <v-col>
-                    <v-card height="497px">
-                        <v-container height="600px" class="secondary">
-                            <font color="white">
-                            <row>
-                                <h1 style="font-size:40px;">Sign Up</h1>
-                            </row>
-                            <v-row>
-                                <v-col cols="12" sm="6">
-                                    <v-text-field
-                                            v-model="first"
-                                            label="First Name"
-                                            outlined
-                                            color="white"
-                                    ></v-text-field>
-
-                                </v-col>
-                                <v-col cols="12" sm="6">
-                                    <v-text-field
-                                            v-model="last"
-                                            label="Last Name"
-                                            outlined
-                                            color="white"
-                                    ></v-text-field>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col>
-                                    <v-text-field
-                                            label="Email address"
-                                            value="example"
-                                            suffix="@gmail.com"
-                                            color="white"
-                                    ></v-text-field>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col>
-                                    <v-text-field
-                                            v-model="password"
-                                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                                            :rules="[rules.required, rules.min]"
-                                            color="white"
-                                            name="input-10-1"
-                                            label="Enter Password"
-                                            hint="At least 8 characters"
-                                            counter
-                                            @click:append="show1 = !show1"
-                                    ></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-text-field
-                                            v-model="confirmPassword"
-                                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                                            :rules="[rules.required, rules.min]"
-                                            color="white"
-                                            name="input-10-1"
-                                            label="Confirm Password"
-                                            hint="At least 8 characters"
-                                            counter
-                                            @click:append="show1 = !show1"
-                                    ></v-text-field>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col>
-                                    <v-checkbox color="white" v-model="readonly" class="mx-2" label="I agree to the Terms and Conditions"></v-checkbox>
-                                </v-col>
-                            </v-row>
-                            </font>
-                            <row>
-                                <v-btn>Sign up</v-btn>
-                            </row>
-                        </v-container>
-                    </v-card>
-                </v-col>
-            </v-row>
+    <v-container justify="center">
+        <v-row>
+            <v-col>
+                <v-card class="accent pa-5 white--text" min-height="100%">
+                    <h1 class="display-1 font-weight-light">Welcome to Help Me Grow</h1>
+                    <p class="font-weight-light">The best place to keep track of your plants</p>
+                    <div>
+                        <v-btn depressed>Already have an account?</v-btn>
+                    </div>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="pa-5 accent darken-1 white--text" min-height="100%">
+                    <h1 class="display-1 font-weight-light">Sign Up</h1>
+                    <p class="font-weight-light">Create a new account for free</p>
+                    <v-row>
+                        <v-col cols="12" sm="6">
+                            <v-text-field dark label="First Name" outlined v-model="first"/>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-text-field dark label="Last Name" outlined v-model="last"/>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-text-field dark label="Email address" outlined/>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-text-field
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                    :rules="[rules.required, rules.min]"
+                                    @click:append="show1 = !show1"
+                                    dark
+                                    hint="At least 8 characters"
+                                    label="Enter Password"
+                                    name="input-10-1"
+                                    outlined
+                                    v-model="password"
+                            />
+                        </v-col>
+                        <v-col>
+                            <v-text-field
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                    :rules="[rules.required, rules.min]"
+                                    @click:append="show1 = !show1"
+                                    dark
+                                    hint="At least 8 characters"
+                                    label="Confirm Password"
+                                    name="input-10-1"
+                                    outlined
+                                    v-model="confirmPassword"
+                            />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-btn class="ml-3" depressed>Sign up</v-btn>
+                    </v-row>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -94,7 +68,7 @@
     //javascript
     export default {
         name: "signup",
-        data () {
+        data() {
             return {
                 // labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
                 // time: 0,
@@ -103,7 +77,8 @@
                 //     { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
                 //     { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
                 // ],
-
+                first: "",
+                last: "",
                 show1: false,
                 show2: true,
                 show3: false,
@@ -111,7 +86,7 @@
                 password: "",
                 confirmPassword: "",
                 rules: {
-                    required: value => !!value || 'Required.',
+                    required: value => !!value || 'Entry required',
                     min: v => v.length >= 8 || 'Min 8 characters',
                     emailMatch: () => ('The email and password you entered don\'t match'),
                 },
