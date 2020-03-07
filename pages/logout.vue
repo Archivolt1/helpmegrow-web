65b97279ef971c849984c02b710a70ebca95e72b
Matthew C. Tran <itgfreak911@gmail.com>
2020-03-07 16:37:07 -0600

    App layouts.

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,11 +1,25 @@
 <template>
-    <div></div>
+
 </template>
 
 <script>
+    //import Cookies from "js-cookie";
+
     export default {
-        name: "logout"
-    }
+        name: "logout",
+        head() {
+            return {
+                title: "Log Out"
+            };
+        },
+        /*fetch() {
+            Cookies.remove("helpmegrow-vuex"); // Remove cookie storing state information.
+            //window.localStorage.removeItem("helpmegrow-vuex"); // Remove token in local storage.
+        },*/
+        mounted: function() {
+            window.location.href = "/signin";
+        }
+    };
 </script>
 
 <style scoped>
