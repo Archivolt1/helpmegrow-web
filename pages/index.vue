17eeee63e13c79f45bc759115f1260c8589955cd
sporkforcereal <juhojung@unomaha.edu>
2020-03-07 13:51:06 -0600

    created index 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-
-    </div>
+    <v-container>
+        <v-row class="font-weight-bold display-2" justify="center">
+            What is Help Me Grow?
+        </v-row>
+        <v-row justify="center">
+            Do you
+        </v-row>
+        <v-row>
+            What is Help Me Grow?
+        </v-row>
+    </v-container>
 </template>
 
+
+
+
+
+
+
 <script>
+    //logic
     export default {
         name: "index"
     }
