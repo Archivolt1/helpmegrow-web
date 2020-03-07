9952da839f25ea09353e5e0d508f890724a519d3
AlfredBalanay <balanayalfred@gmail.com>
2020-03-07 13:02:54 -0600

    SIGNUP PAGE

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,56 +1,8 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    <p class="font-weight-bold">test</p>
-                </v-card>
-            </v-col>
-            <v-col>
-                <v-card>
-                    test
-                </v-card>
-            </v-col>
-        </v-row>
+        <v-card>
+            <p class="font-weight-bold">test</p>
+        </v-card>
     </v-container>
 </template>
 
