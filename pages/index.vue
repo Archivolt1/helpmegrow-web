1b03a8deaa3308a31220f3e0e9f6402db2e13ddd
sporkforcereal <juhojung@unomaha.edu>
2020-03-07 15:18:34 -0600

    created index 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
         <v-row class="font-weight-bold display-2 mb-5" justify="center">
             What is Help Me Grow?
         </v-row>
-        <v-row justify="center">
+        <v-row justify="center" class="mb-1">
             We are a small Indie company who focuses on your plants.
         </v-row>
-        <v-row justify="center">
+        <v-row justify="center" class="mb-1">
             We like plants.
         </v-row>
-        <v-row justify="center">
+        <v-row justify="center" class="mb-1">
             And so should you.
         </v-row>
         <v-row justify="center">
             plz keep your plants alive 🌵
         </v-row>
+        <v-img>
+            <img src="../static/animal crossing.jpg" height="783" width="640"/>
+        </v-img>
     </v-container>
 </template>
 
