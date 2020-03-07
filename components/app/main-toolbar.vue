5e5f778b24b97c813fba3ed6e189ec3b66092896
Matthew C. Tran <itgfreak911@gmail.com>
2020-03-07 13:59:52 -0600

    Initialization of Project.

---
+++ components/app/main-toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-toolbar flat style="border-bottom: 1px #EEEEEE solid;">
+        <v-toolbar-title>Help Me Grow (Replace me with logo)</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn to="/" text>
+            Home
+        </v-btn>
+        <v-btn to="/signup" text>
+            Sign Up
+        </v-btn>
+        <v-btn to="/signin" depressed color="green" class="white--text">
+            Sign In
+        </v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "main-toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
