83b33fc6f16579e839ebf60d4b5edc095482bad6
AlfredBalanay <balanayalfred@gmail.com>
2020-03-07 14:07:03 -0600

    SIGNUP PAGE

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,15 +1,42 @@
 <template>
     <v-container>
-        <v-card>
-            <p class="font-weight-bold">test</p>
-        </v-card>
+        <h1>
+            <v-row>
+                <v-col>
+                    <v-card>
+
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-container>
+                        <row></row>
+                        <row></row>
+                        <row></row>
+                        <row></row>
+                        <row></row>
+                        <row></row>
+                    </v-container>
+                </v-col>
+            </v-row>
+        </h1>
     </v-container>
 </template>
 
 <script>
     //javascript
     export default {
-        name: "signup"
+        name: "signup",
+        data () {
+            return {
+                labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
+                time: 0,
+                forecast: [
+                    { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
+                    { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
+                    { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
+                ],
+            }
+        },
     }
 </script>
 
