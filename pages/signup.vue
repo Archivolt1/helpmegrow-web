a11597fabf2fd383914b673baa07c186d5e73cbf
AlfredBalanay <balanayalfred@gmail.com>
2020-03-07 12:56:03 -0600

    SIGNUP PAGE

---
+++ pages/signup.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    <p class="font-weight-bold">test</p>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    test
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    //javascript
+    export default {
+        name: "signup"
+    }
+</script>
+
+
+<style scoped>
+
+</style>
