5c287f40e02c887bfc96bfd48ccbdd7cd2c7aa35
AlfredBalanay <balanayalfred@gmail.com>
2020-03-07 22:28:15 -0600

    exasperation

---
+++ pages/plants/_id.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card flat dark class="primary pl-5" height="100%" width="100%">
+        <v-container>
+            <v-row>
+                <v-col xs="6">
+                    <v-row>
+                        <p>Summary</p>
+                    </v-row>
+                    <v-row>
+                        <p>Temperature</p>
+                    </v-row>
+                    <v-row>
+                        <p>Indoor/Outdoor/Mobile</p>
+                    </v-row>
+                    <v-row>
+                        <p>Sunlight</p>
+                    </v-row>
+                    <v-row>
+                        <p>Watering frequency</p>
+                    </v-row>
+                </v-col>
+                <v-col xs="6">
+                    <img src="potato.jpg" alt="potato" height="100%" width="100%">
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "_id"
+    }
+</script>
+
+<style scoped>
+
+</style>
