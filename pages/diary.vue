1d137082ff9abc0f2ad6670b3b909c293a604908
AlfredBalanay <balanayalfred@gmail.com>
2020-03-07 21:18:49 -0600

    SIGNUP PAGE

--- pages/diary.vue
+++ pages/diary.vue
@@ -5,20 +5,41 @@
                 <v-col>
                     <v-card class="secondary" dark :align="'center'" width="60%" height="100%">
                         <h1 class="display-1 font-weight-light">New Entry</h1>
-                        <v-text-field
-                                v-model="name"
-                                label="Outlined"
-                                outlined
-                                clearable
-                        ></v-text-field>
-                        <br></br>
-                        <v-text-field
-                                v-model="description"
-                                label="Outlined"
-                                outlined
-                                clearable
-                        ></v-text-field>
+                        <v-container fluid>
 
+                            <v-row>
+                                <v-col cols="4">
+                                    <v-subheader>Entry Title</v-subheader>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-text-field
+                                            outlined
+                                            clearable
+                                            label="Title here"
+                                    ></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="4">
+                                    <v-subheader>Enter Description</v-subheader>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-textarea
+                                            outlined
+                                            clearable
+                                            label="Description"
+                                    ></v-textarea>
+                                </v-col>
+                            </v-row>
+                            </v-container>
+                        <v-file-input
+                                :rules="rules"
+                                accept="image/png, image/jpeg"
+                                placeholder="Upload a plant"
+                                prepend-icon="mdi-camera"
+                                label="Plant"
+                        ></v-file-input>
+                        <v-btn color="white" light>Submit entry</v-btn>
                         <!-- Name of diary, Description, Image upload-->
                     </v-card>
                 </v-col>
@@ -38,6 +59,9 @@
         name: "diary",
         layout: "appDefault",
         data: () => ({
+            rules: [
+                value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
+            ],
             expand: false,
             items: [
                 {
